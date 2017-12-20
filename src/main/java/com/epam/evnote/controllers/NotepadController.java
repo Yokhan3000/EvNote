af508cd14768aa11c04f44e70951d176a6c64319
PharuS <pharus@mail.ru>
2017-12-20 20:17:38 +0300

    merge changes

--- src/main/java/com/epam/evnote/controllers/NotepadController.java
+++ src/main/java/com/epam/evnote/controllers/NotepadController.java
@@ -1,22 +1,15 @@
 package com.epam.evnote.controllers;
-
 import com.epam.evnote.domain.Notepad;
 import com.epam.evnote.domain.User;
 import com.epam.evnote.service.impl.NotepadService;
 import com.epam.evnote.service.impl.UserService;
-import java.util.ArrayList;
-import java.util.Enumeration;
 import java.util.List;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.SessionAttributes;
 
 /**
  * @author Mikhail Chtetsov on 19/12/2017.
@@ -29,7 +22,7 @@ public class NotepadController {
 
   @Autowired
   public NotepadController(NotepadService notepadService,
-      UserService userService) {
+                           UserService userService) {
     this.notepadService = notepadService;
     this.userService = userService;
   }
--- src/main/java/com/epam/evnote/controllers/UserController.java
+++ src/main/java/com/epam/evnote/controllers/UserController.java
@@ -13,15 +13,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author Mikhail Chtetsov on 12/12/2017.
@@ -59,7 +51,8 @@ public class UserController {
   }
 
   @RequestMapping("/info")
-  public @ResponseBody String userInfo(Authentication authentication) {
+  public @ResponseBody
+  String userInfo(Authentication authentication) {
     String msg = "";
     for (GrantedAuthority authority : authentication.getAuthorities()) {
       String role = authority.getAuthority();
